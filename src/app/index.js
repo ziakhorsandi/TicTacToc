f1b614f8b2ec32c5b41d2bd6e20217ac17cdd769
zik <ziakhorsandi2@gmail.com>
2020-03-07 17:46:51 +0330

    deploy version

--- src/app/index.js
+++ src/app/index.js
@@ -12,7 +12,7 @@ class App extends React.Component {
   render() {
     return (
       <Provider>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <main>
             <Switch>
               <Route exact path="/" component={Initial} />
